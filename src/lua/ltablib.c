c6cc8104bdba414a65ad6e3db08e180b227ddd0d
王华侨 <honestqiao@163.com>
2023-07-14 20:12:24 +0800

    Upgrade lua engine from 5.3.5 to 5.3.6

--- src/lua/ltablib.c
+++ src/lua/ltablib.c
@@ -256,13 +256,13 @@ typedef unsigned int IdxT;
 ** is to copy them to an array of a known type and use the array values.
 */
 static unsigned int l_randomizePivot (void) {
-//  clock_t c = clock();
+  // clock_t c = clock();
   time_t t = time(NULL);
-//  unsigned int buff[sof(c) + sof(t)];
+  // unsigned int buff[sof(c) + sof(t)];
   unsigned int buff[sof(t)];
   unsigned int i, rnd = 0;
-//  memcpy(buff, &c, sof(c) * sizeof(unsigned int));
-//  memcpy(buff + sof(c), &t, sof(t) * sizeof(unsigned int));
+  // memcpy(buff, &c, sof(c) * sizeof(unsigned int));
+  // memcpy(buff + sof(c), &t, sof(t) * sizeof(unsigned int));
   memcpy(buff, &t, sof(t) * sizeof(unsigned int));
   for (i = 0; i < sof(buff); i++)
     rnd += buff[i];
